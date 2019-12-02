af2aff2301ac449231433cb69b5c0088aeb7d54e
赵蒙 <wbzhaomeng@centaline.com.cn>
2019-12-02 12:14:09 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import $ from 'jquery';
+import ReactHighcharts from'react-highcharts';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component  {
+  constructor(props) {
+      console.log("constructor");
+      super(props);
+      this.state = {x: '', y: '',timer: null,city: props.city,isLoading: true};
+  }
+ 
+  componentDidMount() {
+    console.log("componentDidMount");
+    var dataSourceUrl = "http://10.58.2.168:9090/getSecondHouseNum?city=" + this.state.city;
+    this.serverRequest = $.get(dataSourceUrl, function (result) {
+        this.setState({
+          x: result.x,
+          y: result.y,
+          isLoading: false
+        });
+      }.bind(this));
+
+    this.state.timer = setInterval(()=>{
+      this.serverRequest = $.get(dataSourceUrl, function (result) {
+        this.setState({
+          x: result.x,
+          y: result.y,
+          isLoading: false
+        });
+      }.bind(this));
+    },1000 * 60 * 10)
+    
+  }
+ componentWillReceiveProps(nextProps){
+  console.log("componentWillReceiveProps");
+  var dataSourceUrl = "http://10.58.2.168:9090/getSecondHouseNum?city=" + nextProps.city;
+  this.serverRequest = $.get(dataSourceUrl, function (result) {
+        this.setState({
+          x: result.x,
+          y: result.y,
+          isLoading: false
+        });
+      }.bind(this));
+  
+ }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount\t' + this.state.city);
+    this.serverRequest.abort();
+    if(this.state.timer!= null) {
+      clearInterval(this.state.timer);
+    }
+  }
+
+  render() {
+    // console.log("render" + this.state.x);
+    var config = {
+      title: {
+        text: '折线图'
+      },
+      subtitle: {
+        text: '数据来源： lianjia.com'
+      },
+      yAxis: {
+        title: {
+          text: '二手房房源数量'
+        }
+      },
+      credits:{
+        enabled:false
+      },
+      xAxis:{
+        categories: this.state.x
+      },
+      series: [{
+        name: '时刻',
+        data: this.state.y
+      }]
+    };
+
+    const showRender = this.state.isLoading;
+    this.state.isLoading = true;
+    return (
+      showRender ? <div /> : <div><ReactHighcharts config={config} /></div>
+    );
+  }
+    
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,43 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App city="bj"/>, document.getElementById('root'));
+
+    class FlavorForm extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {value: 'wh'};
+            this.handleChange = this.handleChange.bind(this);
+        }
+        handleChange(event) {
+            this.setState({value: event.target.value});
+        }
+        render() {
+            return (
+                    <div>
+                        <label>choose favorite city：
+                            <select value={this.state.value} onChange={this.handleChange}>
+                                <option value="wh">武汉</option>
+                                <option value="hz">杭州</option>
+                                <option value="sh">上海</option>
+                                <option value="wx">无锡</option>
+                                <option value="nj">南京</option>
+                                <option value="sz">苏州</option>
+                                <option value="km">昆明</option>
+                                <option value="dali">大理</option>
+                            </select>
+                        </label>
+                        <div>chosen: {this.state.value}</div>
+                        <div>
+                        	<App city={this.state.value}/>
+                        </div>
+                    </div>
+            )
+        }
+    }
+    ReactDOM.render(<FlavorForm/>,document.getElementById('root'))
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
